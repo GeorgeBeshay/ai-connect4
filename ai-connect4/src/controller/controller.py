036f0745830e9a7a7d9f0eb4790f22051d462558
George Beshay <georgebeshay101@gmail.com>
2023-11-27 14:25:12 +0200

    Connect 4 - Initial Implementation

---
+++ ai-connect4/src/controller/controller.py
@@ -0,0 +1,28 @@
+from src.state.state import State
+
+
+class Connect4Controller:
+    """
+    Class representing a Connect 4 game controller.
+    """
+
+    def __init__(self):
+        """
+        Initialize the Connect 4 game controller.
+
+        Initializes the game_state attribute using the State class.
+        """
+        self.game_state = State()
+
+    def play(self, col):
+        """
+        Play a move in the Connect 4 game.
+
+        Args:
+            col (int): The column in which the player wants to place their piece.
+
+        Returns:
+            list: A 2D list representing the updated game state after the move.
+        """
+        self.game_state._update_col(col, True)
+        return self.game_state.to_2d()
