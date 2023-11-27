91a89e9b41fac69c5b328a793173ac5ab83a1a9b
MariamAziz0 <ma27523@gmail.com>
2023-11-27 13:37:38 +0200

    Some comments are added
    The only missing in this implementation is to calculate the heuristic function when the tree reached the max depth aka k

--- ai-connect4/src/algorithms/minimax_with_alpha_beta.py
+++ ai-connect4/src/algorithms/minimax_with_alpha_beta.py
@@ -1,12 +1,24 @@
-class MinimaxWithAlphaBeta:
+from typing import *
+from src.state.state import State
+from src.utilities.get_score import get_computer_score
 
-    # TODO: make state of type state
+
+class MinimaxWithAlphaBeta:
 
     def __init__(self, k: int):
         self.k = k
         self.explored = {}
 
-    def run_minimax_with_alpha_beta(self, initial_state):
+    def run_minimax_with_alpha_beta(self, initial_state: State) -> Tuple[float, State]:
+        """
+
+        Get the maximum value the computer can get from all its children
+
+        :param initial_state: the initial state from which the algorithm runs
+        :type initial_state: State
+
+        :return: Tuple of the max value the computer can get and the state of next step
+        """
         self.explored = {}
         successors = initial_state.get_successors()
         alpha = - float('inf')
@@ -18,30 +30,47 @@ class MinimaxWithAlphaBeta:
                 next_step = successor
                 alpha = current_value
 
-        return next_step
+        return alpha, next_step
 
-    def value(self, state, level: int, alpha: float, beta: float) -> float:
-        if state.get_value() in self.explored:
-            return self.explored[state.get_value()]
+    def value(self, state: State, level: int, alpha: float, beta: float) -> float:
+        """
+        Calculate the value of state
+
+        NOTE: there is explored map where it checks if this value of state has been seen before it returns its value
+              otherwise, the value is computed and then stored in explored map
+
+        :param state: the current state
+        :type state: State
+        :param level: the level or the depth of this state
+        :type level: int
+        :param alpha: the maximum value, this state has seen
+        :type alpha: float
+        :param beta: the minimum value, this state has seen
+        :type beta: float
+        :return: the value of this state
+        """
+        if state.value in self.explored:
+            return self.explored[state.value]
 
         evaluated_value = 0
         if level == self.k:
-            if state.get_value() not in self.explored:
+            if state.value not in self.explored:
                 # evaluated_value = get_evaluation(state)
-                self.explored[state.get_value()] = evaluated_value
+                # evaluated_value = get_computer_score(state.to_2d())
+                self.explored[state.value] = evaluated_value
                 return evaluated_value
             else:
-                return self.explored[state.get_value()]
+                return self.explored[state.value]
 
-        if state.is_max():
+        if state.is_computer_turn:
             evaluated_value = self.max_value(state, level, alpha, beta)
         else:
             evaluated_value = self.min_value(state, level, alpha, beta)
 
-        self[state.get_value()] = evaluated_value
+        self.explored[state.value] = evaluated_value
         return evaluated_value
 
-    def min_value(self, state, level: int, alpha: float, beta: float) -> float:
+    def min_value(self, state: State, level: int, alpha: float, beta: float) -> float:
         v = float('inf')
         successors = state.get_successors()
         for successor in successors:
@@ -51,7 +80,7 @@ class MinimaxWithAlphaBeta:
             beta = min(beta, v)
         return v
 
-    def max_value(self, state, level: int, alpha: float, beta: float) -> float:
+    def max_value(self, state: State, level: int, alpha: float, beta: float) -> float:
         v = - float('inf')
         successors = state.get_successors()
         for successor in successors:
